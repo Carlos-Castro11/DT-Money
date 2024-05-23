839d1ff1f487bfe055d02dd5a314649bee77564a
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-05-23 17:50:40 -0300

    Add modal styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import * as S from './styles';
 import * as Dialog from '@radix-ui/react-dialog';
 import LogoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export function Header() {
   return (
@@ -13,13 +14,7 @@ export function Header() {
             <S.NewTransactionButton>Nova transacao</S.NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>Nova transação</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </S.HeaderContent>
     </S.HeaderContainer>
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,25 @@
+import { X } from 'phosphor-react';
+import * as S from './styles';
+import * as Dialog from '@radix-ui/react-dialog';
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <S.Overlay />
+
+      <S.Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+        <S.CloseButton>
+          <X size={24} />
+        </S.CloseButton>
+        <form>
+          <input type='text' placeholder='Descrição' required />
+          <input type='number' placeholder='Preço' required />
+          <input type='text' placeholder='Categoria' required />
+
+          <button type='submit'>Cadastrar</button>
+        </form>
+      </S.Content>
+    </Dialog.Portal>
+  );
+}
